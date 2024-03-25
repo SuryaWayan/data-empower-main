6683aaccca5fff2d8096811fd42df5b6b266aa58
SuryaWayan <154531900+SuryaWayan@users.noreply.github.com>
2024-03-25 20:03:43 +0800

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -115,73 +115,81 @@ def main():
                         col_a, col_b = st.columns([1.5, 1.5])
                         with col_a:
                             trendline_type = st.selectbox(f"Select trendline type", ["Linear", "Average", "Polynomial"])
-                            with col_b:
-                                if trendline_type == "Polynomial":
-                                    for y_column in y_columns:
-                                        degree = st.number_input(f"Degree for {y_column}", min_value=2, max_value=20, value=2)
+                        with col_b:
+                            if trendline_type == "Polynomial":
+                                degrees = {}
+                                for y_column in y_columns:
+                                    degrees[y_column] = st.number_input(f"Degree for {y_column}", min_value=2, max_value=20, value=2)
 
-                with col_chart:
-                    with st.container():
-                        if x_column and y_columns:
-                            filtered_data = data[(data[x_column] >= x_start) & (data[x_column] <= x_end)]
+                    with col_chart:
+                        with st.container():
+                            if x_column and y_columns:
+                                filtered_data = data[(data[x_column] >= x_start) & (data[x_column] <= x_end)]
 
-                            if chart_type == "Line":
-                                fig = px.line(filtered_data, x=x_column, y=y_columns)
-                            elif chart_type == "Bar":
-                                fig = px.bar(filtered_data, x=x_column, y=y_columns)
-                            else:
-                                fig = px.scatter(filtered_data, x=x_column, y=y_columns)
+                                if chart_type == "Line":
+                                    fig = px.line(filtered_data, x=x_column, y=y_columns)
+                                elif chart_type == "Bar":
+                                    fig = px.bar(filtered_data, x=x_column, y=y_columns)
+                                else:
+                                    fig = px.scatter(filtered_data, x=x_column, y=y_columns)
 
-                            if trendline:
+                                if trendline:
+                                    for trace in fig.data:
+                                        if trace.name in y_columns:
+                                            color = trace.line.color if hasattr(trace.line, 'color') else trace.marker.color
+                                            if trendline_type == "Linear":
+                                                slope, intercept, _, _, _ = linregress(filtered_data[x_column], filtered_data[trace.name])
+                                                fig.add_shape(type="line", x0=x_start, y0=slope*x_start+intercept,
+                                                            x1=x_end, y1=slope*x_end+intercept,
+                                                            line=dict(color=color, width=2, dash="dash"))
+                                                y_predicted = slope * filtered_data[x_column] + intercept
+                                                r_squared = r2_score(filtered_data[trace.name], y_predicted)
+                                            elif trendline_type == "Average":
+                                                avg_y_value = filtered_data[trace.name].mean()
+                                                fig.add_hline(y=avg_y_value, line_dash="dash",
+                                                            annotation_text=f"Avg {trace.name} = {avg_y_value:.5f}",
+                                                            annotation_position="bottom right",
+                                                            line=dict(color=color)) 
+                                            elif trendline_type == "Polynomial":
+                                                degree = degrees[trace.name]
+                                                coeffs = np.polyfit(filtered_data[x_column], filtered_data[trace.name], degree)
+                                                poly_function = np.poly1d(coeffs)
+                                                equation = " + ".join(f"{coeffs[i]:.8f} * x^{degree-i}" for i in range(degree+1))
+                                                x_values = np.linspace(x_start, x_end, 100)
+                                                y_values = poly_function(x_values)
+                                                r_squared = r2_score(filtered_data[trace.name], poly_function(filtered_data[x_column]))
+                                                fig.add_trace(go.Scatter(x=x_values, y=y_values, line_dash="dash",
+                                                                        name=f"Polynomial Trendline {degree} for {trace.name}",
+                                                                        line=dict(color=color))) 
+
+                                fig.update_layout(title=f"Chart {i+1}", xaxis_title=x_column, yaxis_title="Value", height=700) 
+                                st.plotly_chart(fig, use_container_width=True)
+                                st.markdown('<div class="chart-container"></div>', unsafe_allow_html=True)
+
+                with col2:
+                    st.write(f"**Chart {i+1} Summary:**")
+                    st.write("")
+                    st.write("")
+                    
+                    for y_column in y_columns:
+                        st.write("• **Min of**", y_column + ":", f"{filtered_data[y_column].min():.5f}")
+                        st.write("• **Max of**", y_column + ":", f"{filtered_data[y_column].max():.5f}")
+                        st.write("• **Average of**", y_column + ":", f"{filtered_data[y_column].mean():.5f}")
+                        st.write("• **Standard Deviation of**", y_column + ":", f"{filtered_data[y_column].std():.5f}")
+                    if trendline:
                                 for trace in fig.data:
                                     if trace.name in y_columns:
-                                        color = trace.line.color if hasattr(trace.line, 'color') else trace.marker.color
                                         if trendline_type == "Linear":
-                                             slope, intercept, _, _, _ = linregress(filtered_data[x_column], filtered_data[trace.name])
-                                             fig.add_shape(type="line", x0=x_start, y0=slope*x_start+intercept,
-                                                           x1=x_end, y1=slope*x_end+intercept,
-                                                           line=dict(color=color, width=2, dash="dash"))
-                                             y_predicted = slope * filtered_data[x_column] + intercept
-                                             r_squared = r2_score(filtered_data[trace.name], y_predicted)
-                                        elif trendline_type == "Average":
-                                            avg_y_value = filtered_data[trace.name].mean()
-                                            fig.add_hline(y=avg_y_value, line_dash="dash",
-                                                        annotation_text=f"Avg {trace.name} = {avg_y_value:.5f}",
-                                                        annotation_position="bottom right",
-                                                        line=dict(color=color)) 
+                                            st.write(f"• **Linear Trendline for {trace.name}:** y = {slope:.5f}x + {intercept:.5f}, R-squared: {r_squared:.5f}")
                                         elif trendline_type == "Polynomial":
+                                            degree = degrees[trace.name]
                                             coeffs = np.polyfit(filtered_data[x_column], filtered_data[trace.name], degree)
                                             poly_function = np.poly1d(coeffs)
                                             equation = " + ".join(f"{coeffs[i]:.8f} * x^{degree-i}" for i in range(degree+1))
                                             x_values = np.linspace(x_start, x_end, 100)
                                             y_values = poly_function(x_values)
                                             r_squared = r2_score(filtered_data[trace.name], poly_function(filtered_data[x_column]))
-                                            fig.add_trace(go.Scatter(x=x_values, y=y_values, line_dash="dash",
-                                                                    name=f"Polynomial Trendline {degree} for {trace.name}",
-                                                                    line=dict(color=color))) 
-
-                            fig.update_layout(title=f"Chart {i+1}", xaxis_title=x_column, yaxis_title="Value", height=700) 
-                            st.plotly_chart(fig, use_container_width=True)
-                            st.markdown('<div class="chart-container"></div>', unsafe_allow_html=True)
-
-                with col2:
-                    st.write(f"**Chart {i+1} Summary:**")
-                    st.write("")
-                    st.write("")
-                    for y_column in y_columns:
-                        st.write("• **Min of**", y_column + ":", f"{filtered_data[y_column].min():.5f}")
-                        st.write("• **Max of**", y_column + ":", f"{filtered_data[y_column].max():.5f}")
-                        st.write("• **Average of**", y_column + ":", f"{filtered_data[y_column].mean():.5f}")
-                        st.write("• **Standard Deviation of**", y_column + ":", f"{filtered_data[y_column].std():.5f}")
-                        if trendline:
-                                for trace in fig.data:
-                                    if trace.name in y_columns:
-                                        if trendline_type == "Linear":
-                                            st.write(f"• **Linear Trendline for {trace.name}:** y = {slope:.5f}x + {intercept:.5f}, R-squared: {r_squared:.5f}")
-                                        elif trendline_type == "Polynomial":
-                                            st.write(f"• **Polynomial Trendline {degree} for {trace.name}:** y = {equation}, R-squared: {r_squared:.5f}")
-                    st.write("")
-                    st.write("")
+                                            st.write(f"• **Polynomial Trendline {degree} for {trace.name}:** y = {equation}, R-squared: {r_squared:.5f}")    
 
 # Run the app
 if __name__ == "__main__":
